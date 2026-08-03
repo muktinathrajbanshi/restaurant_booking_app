668ffe72c6965a289c4f7817a3f8ca9d0d0e7b8d
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-08-03 22:06:06 +0545

    hait man kam dhama dham

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,4 +1,14 @@
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { User } from "../models/User.js";
+import bcrypt from "bcrypt";
+
+// Helper to generate JWT token
+const generateToken = (id: string) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET as string, {
+    expiresIn: "30d",
+  });
+};
 
 // Register a new user
 // POST /api/auth/register
@@ -7,7 +17,49 @@ export const registerUser = async (
   res: Response,
 ): Promise<void> => {
   try {
-  } catch (error) {}
+    const { name, email, password, phone, role } = req.body;
+
+    if (!name || !email || !password) {
+      res.status(400).json({ message: "Please enter all required fields" });
+      return;
+    }
+
+    // check if user exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400).json({ message: "User already exists" });
+      return;
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      phone,
+      role,
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        role: user.role,
+        token: generateToken(user._id.toString()),
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error: any) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
 };
 
 // Authenticate a user & get token
