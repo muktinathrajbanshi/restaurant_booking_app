74b36d93771e46f453b9773c0569b2ec5194dded
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-08-09 20:22:09 +0545

    let's get started

---
+++ server/routes/bookingRoutes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { protect } from "../middlewares/auth.js";
+import {
+  cancelBooking,
+  createBooking,
+  getMyBookings,
+} from "../controllers/bookingController.js";
+
+const bookingRouter = Router();
+
+bookingRouter.post("/", protect, createBooking);
+bookingRouter.get("/", protect, getMyBookings);
+bookingRouter.put("/:id/cancel", protect, cancelBooking);
+
+export default bookingRouter;
