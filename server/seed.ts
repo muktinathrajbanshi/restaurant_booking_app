a333d28c9950de31e73a65123d383c0294eb15a4
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-08-13 11:57:02 +0545

    working on database management role like user, admin, owner

--- server/seed.ts
+++ server/seed.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 import { User } from "./models/User.js";
 import { Restaurant } from "./models/Restaurant.js";
 import { Booking } from "./models/Booking.js";
@@ -17,6 +18,31 @@ const seedData = async () => {
     await User.deleteMany({});
     await Restaurant.deleteMany({});
     await Booking.deleteMany({});
+
+    console.log("Creating default users...");
+
+    const salt = await bcrypt.genSalt(10);
+    const adminPassword = await bcrypt.hash("admin143", salt);
+    const userPassword = await bcrypt.hash("user143", salt);
+    const ownerPassword = await bcrypt.hash("owner143", salt);
+
+    // Admin
+    const adminUser = await User.create({
+      name: "Muktinath Rajbanshi",
+      email: "muktinathrajbanshi786@gmail.com",
+      password: adminPassword,
+      phone: "+9779824929410",
+      role: "admin",
+    });
+
+    // User
+    const testUser = await User.create({
+      name: "Milan Rajbanshi",
+      email: "user123@gmail.com",
+      password: userPassword,
+      phone: "+9779824929410",
+      role: "user",
+    });
   } catch (error: any) {
     console.error("Seeding failed:", error);
     process.exit(1);
