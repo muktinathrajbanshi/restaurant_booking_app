6113144d3fc49c9f3bf0678c34412587f75d51d8
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-08-20 22:10:16 +0545

    now it's time for real final output

--- server/controllers/ownerController.ts
+++ server/controllers/ownerController.ts
@@ -5,47 +5,18 @@ import { v2 as cloudinary } from "cloudinary";
 import { Booking } from "../models/Booking.js";
 
 // Helper function to upload buffer to Cloudinary
-// const uploadToCloudinary = (
-//   fileBuffer: Buffer,
-// ): Promise<{ secure_url: string }> => {
-//   return new Promise((resolve, reject) => {
-//     const stream = cloudinary.uploader.upload_stream(
-//       { folder: "QuickDine" },
-//       (error, result) => {
-//         if (error) return reject(error);
-//         if (!result) return reject(new Error("Upload failed"));
-//         resolve({ secure_url: result.secure_url });
-//       },
-//     );
-//     stream.end(fileBuffer);
-//   });
-// };
-
 const uploadToCloudinary = (
   fileBuffer: Buffer,
 ): Promise<{ secure_url: string }> => {
   return new Promise((resolve, reject) => {
-    console.log("Uploading image to Cloudinary...");
-    console.log("Buffer size:", fileBuffer.length);
-
     const stream = cloudinary.uploader.upload_stream(
       { folder: "QuickDine" },
       (error, result) => {
-        if (error) {
-          console.error("CLOUDINARY ERROR:", error);
-          return reject(error);
-        }
-
-        if (!result) {
-          return reject(new Error("Upload failed"));
-        }
-
-        console.log("Cloudinary upload successful");
-
+        if (error) return reject(error);
+        if (!result) return reject(new Error("Upload failed"));
         resolve({ secure_url: result.secure_url });
       },
     );
-
     stream.end(fileBuffer);
   });
 };
@@ -260,7 +231,7 @@ export const updateBookingStatus = async (
 ): Promise<void> => {
   try {
     const { status } = req.body;
-    if (!status || !["confirmed", "completed", "completed"].includes(status)) {
+    if (!status || !["confirmed", "cancelled", "completed"].includes(status)) {
       res.status(400).json({ message: "Please enter a valid booking status" });
       return;
     }
